57acf3e7649629239b7b335b8ecc0356a4f4534c
Kushang Shah <kushangshah131@gmail.com>
2020-07-19 14:00:07 +0530

    Update kel.js

--- kel.js
+++ kel.js
@@ -3,7 +3,7 @@
     window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-var canvas = document.getElementById("canvas"),
+	var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
     width = 1000,
     height = 400,
@@ -19,13 +19,14 @@ var canvas = document.getElementById("canvas"),
         grounded: false,
         color:'#E6AC27'
     },
+	
     keys = [],
     friction = 0.8,
     gravity = 0.4,
     boxes = [],
-    powerup = [];  
+    powerup = []; 
 
-powerup.push({
+	powerup.push({
         x: 810,
         y: 250,
         width: 20,
@@ -33,7 +34,7 @@ powerup.push({
         color: '#BF4D28',
         effect: 'shrink'
     });
-powerup.push({
+	powerup.push({
         x: 400,
         y: 150,
         width: 20,
@@ -41,7 +42,7 @@ powerup.push({
         color: '#BF4D28',
         effect: 'gravity'
     });
-powerup.push({
+	powerup.push({
         x: -15,
         y: 88,
         width: 20,
@@ -53,7 +54,7 @@ powerup.push({
         py: 370,
         stay: true
     });
-powerup.push({
+	powerup.push({
         x: 60,
         y: 365,
         width: 20,
@@ -63,3 +64,95 @@ powerup.push({
         stay: true
     });
 
+// dimensions
+boxes.push({
+	//box on left
+    x: 0,
+    y: height/4+10,
+    width: 10,
+    height: height,
+    color: 'green'
+});
+boxes.push({
+	//box on left
+    x: 0,
+    y: 0,
+    width: 10,
+    height: height/4-15,
+    color: 'green'
+});
+boxes.push({
+	//box for the ground
+    x: 0,
+    y: height - 10,
+    width: width,
+    height: 50,
+    color: 'orange'
+});
+boxes.push({
+	//box on right
+    x: width - 10,
+    y: 0,
+    width: 50,
+    height: height,
+    color: 'yellow'
+});
+boxes.push({
+    x: 290,
+    y: 200,
+    width: 260,
+    height: 10,
+    color: 'blue'
+});
+boxes.push({
+    x: 590,
+    y: 200,
+    width: 80,
+    height: 10,
+    color: 'blue'
+});
+boxes.push({
+    x: 120,
+    y: 250,
+    width: 150,
+    height: 10,
+    color: 'red'
+});
+boxes.push({
+    x: 220,
+    y: 300,
+    width: 80,
+    height: 10,
+    color: 'black'
+});
+boxes.push({
+    x: 340,
+    y: 350,
+    width: 90,
+    height: 10,
+    color: '#655643'
+});
+boxes.push({
+    x: 740,
+    y: 300,
+    width: 160,
+    height: 10,
+    color: '#655643'
+});
+boxes.push({
+    x: 0,
+    y: 350,
+    width: 90,
+    height: 10,
+    color: '#655643'
+});
+boxes.push({
+    x: 90,
+    y: 350,
+    width: 10,
+    height: 50,
+    color: '#655643'
+});
+
+canvas.width = width;
+canvas.height = height;
