c169d0f60fa25d21d4ea9ac807d192856f8150dd
Kushang Shah <kushangshah131@gmail.com>
2020-07-21 15:15:34 +0530

    Update kel.js

--- kel.js
+++ kel.js
@@ -170,15 +170,66 @@ function update() {
 	// check keys
     if (keys[38] || keys[32] || keys[87]) {
         // up arrow or space
-        
+		alert("Herre");
+        if (!player.jumping && player.grounded) {
+            player.jumping = true;
+            player.grounded = false;
+            //how high to jump
+			player.velY = -player.speed * 2.5;
+        }
     }
     if (keys[39] || keys[68]) {
         // right arrow
-        
+        if (player.velX < player.speed) {
+            player.velX++;
+        }
     }
     if (keys[37] || keys[65]) {
         // left arrow
-        
+        if (player.velX > -player.speed) {
+            player.velX--;
+        }
     }
 	
+<<<<<<< Updated upstream
+=======
+	player.velX *= friction;
+    player.velY += gravity;
+
+    ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+    
+    player.grounded = false;
+	for (var i = 0; i < boxes.length; i++) {//print boxes
+        ctx.fillStyle = boxes[i].color;
+        ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+        
+        var dir = colCheck(player, boxes[i]);
+
+        if (dir === "l" || dir === "r") {
+            player.velX = 0;
+            player.jumping = false;
+        } else if (dir === "b") {
+            player.grounded = true;
+            player.jumping = false;
+        } else if (dir === "t") {
+            player.velY *= -1;
+        }
+
+    }
+	
+	
+	if(player.grounded){
+         player.velY = 0;
+    }
+    
+    player.x += player.velX;
+    player.y += player.velY;
+  
+    ctx.fill();//Drawing charater stuff
+    ctx.fillStyle = player.color;
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+    
+	
+>>>>>>> Stashed changes
 }
